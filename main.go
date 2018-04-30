044c60090d18e0eb5f9c4f40d4376a01dbea5e70
Sevak Mardirosian <smardirosians@gmail.com>
2018-04-30 10:00:18 +0200

    update function

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstname   string
+	lastname    string
+	contactInfo contactInfo
+}
+
 func main() {
-	cards := newDeck()
-	cards.shuffle()
-	cards.print()
+	// cards := newDeck()
+	// cards.shuffle()
+	// cards.print()
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func log(msg string) {
+	fmt.Println(msg)
+}
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstname = newFirstName
 }
